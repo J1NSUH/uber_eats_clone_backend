5ed436987082502d18ed6dcdca9384046c0eafb9
J1NSUH <j1nsuh@gmail.com>
2024-04-22 15:24:04 +0900

    2-3

---
+++ dist/restaurants/restaurants.resolver.d.ts
@@ -0,0 +1,4 @@
+import { Restaurant } from './entities/restaurant.entity';
+export declare class RestaurantResolver {
+    restaurants(veganOlny: boolean): Restaurant[];
+}
--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,12 +1,16 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql';
 import { Restaurant } from './entities/restaurant.entity';
 
 @Resolver((of) => Restaurant)
 export class RestaurantResolver {
-  @Query((returns) => Restaurant)
-  myRestaurant() {
-    return true;
+  @Query((returns) => [Restaurant]) //"[]"는 graephql 규칙
+  restaurants(@Args('veganOnly') veganOlny: boolean): Restaurant[] {
+    console.log(veganOlny);
+    return [];
   }
+  /*myRestaurant() {
+    return true;
+  }//2.3*/
   /*
     @Query(returns=>Boolean)
     isPizzaGood():Boolean{
