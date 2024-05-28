683b6631201a4392e68fff5c6f49241df76d5443
J1N5UH <jinsuh8375@gmail.com>
2024-05-28 10:18:46 +0900

    .

--- src/restaurants/restaurants.module.ts
+++ src/restaurants/restaurants.module.ts
@@ -3,8 +3,9 @@ import { RestaurantResolver } from './restaurants.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Restaurant } from './entities/restaurant.entity';
 import { RestaurantService } from './restaurants.service';
-
 @Module({
+  // TypeOrmModule.forRoot는 애플리케이션 전체에서 사용할 데이터베이스 연결을 설정합니다.
+  // TypeOrmModule.forFeature는 특정 모듈에서 사용할 엔티티 리포지토리를 설정합니다.
   imports: [TypeOrmModule.forFeature([Restaurant])],
   providers: [RestaurantResolver, RestaurantService],
 })
