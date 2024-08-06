cea6f7a908b7d9997c51d9a055965699e54f6378
J1N5UH <jinsuh8375@gmail.com>
2024-08-06 13:53:00 +0900

    5-2

---
+++ dist/users/users.resolver.d.ts
@@ -0,0 +1,6 @@
+import { UsersService } from './user.service';
+export declare class UsersResolver {
+    private readonly usersService;
+    constructor(usersService: UsersService);
+    hi(): boolean;
+}
